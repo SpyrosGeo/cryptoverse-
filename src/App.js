1bf89c3afa60c0f3d16d5db8689fcf1a1b2b677c
SpyrosGeo <xarodasimba@gmail.com>
2021-09-24 15:15:35 +0300

    added basic css and basic components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import {Switch,Route,Link} from 'react-router-dom';
-import {Layout,Typography,Space} from 'antd'
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import "./App.css";
+import { NavBar,Exchanges,HomePage,News,CryptoDetails,Cryptocurrencies } from "./components";
 function App() {
   return (
     <div className="app">
       <div className="navbar">
-
+        <NavBar />
       </div>
       <div className="main">
-
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
-      <div className="footer">
-
-      </div>
+      <div className="footer"></div>
     </div>
   );
 }
