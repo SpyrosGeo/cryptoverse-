29319cd60153ed58a0f1298c66d3e4ec50e9ab9a
SpyrosGeo <xarodasimba@gmail.com>
2021-11-15 00:56:48 +0200

    added fetching data from the api

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,23 +2,27 @@ import React from "react";
 import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
 const HomePage = () => {
+  const {data,isFetching} = useGetCryptosQuery()
+  const globalStats = data?.data.stats
+  console.log(data)
+  if(isFetching) return 'Loading...'
   return (
     <>
       <Title level={2} className="heading">
         Global Crypto Stats
       </Title>
       <Row>
-          <Col span={12}>
-              <Statistic title="Total Cryptocurrencies" value="5"/>
-              <Statistic title="Total exchanges" value="5"/>
-              <Statistic title="Total Market Cap" value="5"/>
-              <Statistic title="Total 24h Volume" value="5"/>
-              <Statistic title="Total Markets" value="5"/>
-
-          </Col>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+          {/* <Statistic title="Total exchanges" value={millify(globalStats.exchanges)} /> */}
+          {/* <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} />
+          <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} />
+          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /> */}
+        </Col>
       </Row>
     </>
   );
