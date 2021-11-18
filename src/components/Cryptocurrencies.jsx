dec1a2e7451fb2e49f0e62276b48bc44ee05d79f
SpyrosGeo <xarodasimba@gmail.com>
2021-11-18 19:57:03 +0200

    added search bar and functionality on cryptocurrencie page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
@@ -8,14 +8,34 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFethcing } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([])
+  const [searchTerm, setSearchTerm] = useState("");
 
-if(isFethcing){
-   return "loading..."
-}
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setCryptos(filteredData)
+  }, [cryptosList, searchTerm]);
+
+  if (isFethcing) {
+    return "loading...";
+  }
 
   return (
     <>
+    {!simplified &&(
+        
+      <div className="search-crypto">
+        <Input
+          value={searchTerm}
+          placeholder="Search Cryptocurency"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    )}
+      
       <Row className="crypto-card-container" gutter={[32, 32]}>
         {cryptos?.map((currency) => (
           <Col key={currency.id} xs={24} sm={12} lg={6} className="crypto-card">
